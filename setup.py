9c184a82ba24bca18067dc320d489b5b85402ac7
Jayesh <jgadhari5@gmail.com>
2025-07-10 20:47:18 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Information Retrieval System',
+    version= '0.0.0',
+    author= 'Jayesh',
+    author_email= 'jaygadhari32@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
